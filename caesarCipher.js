ce3c61aec966cac912676401247c5708e4d6c899
Sean <seanpoulter@hotmail.com>
2022-11-03 19:15:34 -0400

    Complete cipher and test

--- caesarCipher.js
+++ caesarCipher.js
@@ -3,10 +3,25 @@ function caesar(string, cipher) {
     let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
         'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u'
         , 'v', 'w', 'x', 'y', 'z']
+    let key = cipher
 
-
-
-
+    for (let i = 0; i < string.length; i++) {
+        for (let j = 0; j < alphabet.length; j++) {
+            if (string[i] === ' ') {
+                codedWord += ' '
+                break
+            }
+            if (string[i] === alphabet[j]) {
+                if ((j + key) > alphabet.length - 1) {
+                    codedWord += alphabet[(j + key) - alphabet.length]
+                    break;
+                }
+                codedWord += alphabet[j + key]
+                break;
+            }
+        }
+    }
+    return codedWord
 }
 
 module.exports = caesar
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,2 +1,7 @@
 const caesarCipher = require('../caesarCipher')
 
+describe('caesarCipher', () => {
+    test('encrypt phrase', () => {
+        expect(caesarCipher("encoded words",1)).toBe("fodpefe xpset")
+    })
+})
